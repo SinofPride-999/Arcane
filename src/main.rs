2e708f587356446ff51f5602ce8562c2c3ba2df0
SinofPride-999 <jhaycodes999@gmail.com>
2025-11-25 23:13:04 +0000

    chore: prj setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+
+    let res = reqwest::get("https://youtube.com")
+        .await?
+        .text()
+        .await?;
+
+    println!("{}", res);
+
+    log::info!("Hello World!");
+    Ok(())
 }
